a07930111298ca4480ee690ea62469fb0e1be479
hagybrian <brianhagy@gmail.com>
2017-11-15 20:40:08 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -7,19 +7,18 @@ var path = require("path");
 var app = express();
 var PORT = 3500;
 
-var reservations = [
-
+var reservations = [ 
   {
-    name: "",
-    phoneNumber: "",
-    email: "",
-    uniqueId: ""
-  },
-
+    name: "dummy",
+    phoneNumber: "dumb",
+    email: "d@b.com",
+    uniqueId: "yoyo"
+  }
 ];
 
-reservations
-
+app.get("/reservations", function(req, res) {
+  res.json(reservations);
+});
 
 // Routes to html files
 app.get("/", function(req, res) {
@@ -34,6 +33,19 @@ app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newreservation = req.body;
+
+  console.log(newreservation);
+
+  // We then add the json the user sent to the character array
+  reservations.push(newreservation);
+
+  // We then display the JSON to the users
+  res.json(newreservation);
+});
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
