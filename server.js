5b7989aa701b5ef7465271bf026994a617dd90e1
Roverkim <ryankim@hotmail.com>
2017-11-15 20:35:55 -0600

    update

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ var reservations = [
 
 ];
 
+reservations
+
+
 // Routes to html files
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
@@ -30,7 +33,6 @@ app.get("/tables", function(req, res) {
 app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
-
 
 
 // Starts the server to begin listening
